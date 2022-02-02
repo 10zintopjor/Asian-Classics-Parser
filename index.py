1cf1b68b73f129e6ca9c84cd43228413cbb753b0
jungtop <tenzintopjor27@gmail.com>
2022-02-02 15:07:10 +0530

    fixed some structures

--- index.py
+++ index.py
@@ -57,18 +57,23 @@ def get_meta():
         initial_creation_type=InitialCreationEnum.input,
         created_at=datetime.now(),
         last_modified_at=datetime.now(),
-        source_metadata=vol_no_name_map)
+        source_metadata={"volume_no_to_title":vol_no_name_map})
 
     return instance_meta
 
 def get_file_name(dirpath):
+    
     path = ""
     while os.path.basename(dirpath) != par_dir:
+        if os.path.basename(dirpath) == "":
+            break
         path=os.path.basename(dirpath)+"/"+path if path != "" else os.path.basename(dirpath)[0:20]
         dirpath=dirpath.replace(f"/{os.path.basename(dirpath)}","")
-    return path.replace(" ","_")
+        
+    path =  path.replace("  ","_")
 
-
+    return path.replace(r"TENGYUR (Tibetan Letters)/","")
+    
 def get_base_text(path):
     try:
         with open(path,encoding="UTF-16LE") as f:
@@ -80,13 +85,15 @@ def get_base_text(path):
 
 def create_opf(base_text,filename,pecha_id):
     global vol
-    print(filename)
+
     opf_path=f"./opfs/{pecha_id}/{pecha_id}.opf"
     opf = OpenPechaFS(opf_path=opf_path)
     new_filename = str("{:0>3d}".format(int(vol)))
     vol_no_name_map.update({f"v{new_filename}":f"{filename}"})
+    pagination_layer,base_text = get_pagination_layer(base_text)
+    layers = {f"v{new_filename}": {LayerEnum.pagination: pagination_layer}}
     base = {f"v{new_filename}":base_text}
-    layers = {f"v{new_filename}": {LayerEnum.pagination: get_pagination_layer(base_text)}}
+
 
     opf.layers = layers
     opf.base = base
@@ -95,37 +102,49 @@ def create_opf(base_text,filename,pecha_id):
     vol+=1
 
 def get_pagination_layer(base_text):
+    
 
     page_annotations = {}
     char_walker = 0
-
     formatted_text = format_text(base_text)
+    base_text = ""
+
 
     for text in formatted_text:
-        lot = len(text)
-        page_annotation, char_walker = get_page_annotation(text, char_walker)
+        page_annotation, char_walker,text = get_page_annotation(text, char_walker)
+        base_text += text +"\n\n"
         page_annotations.update(page_annotation)
 
     pagination_layer = Layer(
         annotation_type=LayerEnum.pagination, annotations=page_annotations
     )
 
-    return pagination_layer
+    return pagination_layer,base_text
 
 def get_page_annotation(text,char_walker):
 
     imgnum = get_imgnum(text)
+    text = re.sub("\[.*.\]","",text)
+    text=text.strip()
 
     page_annotation = {
-        uuid4().hex: Page(span=Span(start=char_walker, end=char_walker+len(text)), metadata={"imgnum":imgnum})
+        uuid4().hex: Page(span=Span(start=char_walker, end=char_walker+len(text)), imgnum=imgnum)
     } 
 
-    return page_annotation,(char_walker + len(text) + 2)   
+    return page_annotation,(char_walker + len(text) + 2),text  
 
 def get_imgnum(text):
     text=text.replace("[?]","")
     img = re.search("(\[.*\])?(.*)",text,re.DOTALL)
-    imgnum = re.search("\[(.*)\](.*)?",img.group(1)).group(1) if img.group(1) != None else img.group(1)
+    if img.group(1) != None:
+        imgnum = re.search("\[(.*)\](.*)?",img.group(1)).group(1)
+        img_no = re.search("(\d*)(\D*)",imgnum).group(1)
+        img_alp = re.search("(\d*)(\D*)",imgnum).group(2)
+        img_no = re.sub("^0+(?!$)", "", img_no)
+        imgnum = int(img_no)*2-1 if img_alp == "A" else int(img_no)*2
+
+    else:
+        imgnum = img.group(1)
     return imgnum
 
 def format_text(base_text):
