3c7605fd2a1f0b6f2b638a38d5abd38183327b67
jungtop <tenzintopjor27@gmail.com>
2022-02-11 16:34:41 +0530

    Added token in env

--- test.py
+++ test.py
@@ -1,7 +1,3 @@
 import os
 
-for root, dirs, files in sorted(os.walk("./temp")):
-    if "RTF" in dirs:
-        dirs.remove("RTF")
-    for file in files:
-        print(f"Dealing with file {root}/{file}")
+print(os.environ.get("GITHUB_TOKEN"))
